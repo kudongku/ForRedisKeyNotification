27fef860597417e9e74a1b7a8b3567ded49bff76
kudongku <kudongku@naver.com>
2024-04-08 11:49:29 +0900

    :tada: feat(git) : init commit

---
+++ src/main/java/com/example/forrediskeynotification/ForRedisKeyNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.forrediskeynotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForRedisKeyNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForRedisKeyNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forrediskeynotification/ForRedisKeyNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forrediskeynotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForRedisKeyNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
